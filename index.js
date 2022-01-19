8a088c8cc64e18de4de13c7455dfe83355a47c81
Alexander Milanovich <alexandermilanovich63@gmail.com>
2022-01-19 20:16:08 +0000

    Cleaning Code

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 // Require the necessary discord.js classes
-const { Client, Intents, MessageEmbed } = require('discord.js');
-const { token, idAPP, metarid } = require('./config.json')
+const { Client, Intents, MessageEmbed } = require("discord.js");
+const { token, idAPP, metarid } = require("./config.json")
 
 
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+client.once("ready", () => {
+	console.log("Ready!");
 	client.user.setPresence({
 		activities: [{
 			name: "The Weather",
@@ -21,52 +21,52 @@ client.once('ready', () => {
 
 });
 
-var weather = require('openweather-apis');
+var weather = require("openweather-apis");
 
-weather.setLang('en');
+weather.setLang("en");
 
 // set city by name
-//weather.setCity('Toronto');
+//weather.setCity("Toronto");
 
-// 'metric'  'internal'  'imperial'
-weather.setUnits('metric');
+// "metric"  "internal"  "imperial"
+weather.setUnits("metric");
 
 // check http://openweathermap.org/appid#get for get the idAPP
 weather.setAPPID(idAPP);
 
 
-client.on('interactionCreate', async interaction => {
+client.on("interactionCreate", async interaction => {
 	if (!interaction.isCommand()) {
 		return;
 	}
 	const { commandName, options } = interaction;
 
 	//Premade command specific to Toronto
-	if (commandName === 'toronto') {
+	if (commandName === "toronto") {
 
-		weather.setCity('Toronto');
+		weather.setCity("Toronto");
 		weather.getTemperature(function (err, temp) {
 			console.log(temp);
-			interaction.reply('The Current temperature in Toronto is: ' + temp + 'C');
+			interaction.reply("The Current temperature in Toronto is: " + temp + "C");
 		});
 
 
 	}
 
 	//Premade command specific to Montreal
-	if (commandName === 'montreal') {
-		weather.setCity('Montreal');
+	if (commandName === "montreal") {
+		weather.setCity("Montreal");
 
 		weather.getTemperature(function (err, temp) {
 			console.log(temp);
-			interaction.reply('The Current temperature in Montreal is: ' + temp + 'C');
+			interaction.reply("The Current temperature in Montreal is: " + temp + "C");
 		});
 	}
 
 	
 	//Weather Command. Gets user input, pharses it through the weathermap API, returns temperature value
-	// if (commandName === 'weather') {
-	// 	const string = options.getString('city');
+	// if (commandName === "weather") {
+	// 	const string = options.getString("city");
 
 	// 	await interaction.deferReply({
 	// 	  ephemeral: true,
@@ -82,31 +82,31 @@ client.on('interactionCreate', async interaction => {
 
 	// 		if (temp > 20) {
 	// 			interaction.editReply({
-	// 				content: 'Holy hell its warm in ' + string + ' is: ' + temp + 'C',
+	// 				content: "Holy hell its warm in " + string + " is: " + temp + "C",
 	// 			})
 	// 		}
 
 	// 		if (temp > 10 && temp < 20) {
 	// 			interaction.editReply({
-	// 				content: 'Its warm in ' + string + ' is: ' + temp + 'C',
+	// 				content: "Its warm in " + string + " is: " + temp + "C",
 	// 			})
 	// 		}
 
 	// 		if (temp > 0 && temp < 10) {
 	// 			interaction.editReply({
-	// 				content: 'Its cold in ' + string + ' is: ' + temp + 'C',
+	// 				content: "Its cold in " + string + " is: " + temp + "C",
 	// 			})
 	// 		}
 
 	// 		if (temp < 0 && temp > -10) {
 	// 			interaction.editReply({
-	// 				content: 'Its freezing in ' + string + '. Its ' + temp + 'C',
+	// 				content: "Its freezing in " + string + ". Its " + temp + "C",
 	// 			})
 	// 		}
 
 	// 		if (temp < -10) {
 	// 			interaction.editReply({
-	// 				content: 'Jesus fuck its cold in ' + string + '. Its ' + temp + 'C',
+	// 				content: "Jesus fuck its cold in " + string + ". Its " + temp + "C",
 	// 			})
 	// 		}
 	// 	});
@@ -115,8 +115,8 @@ client.on('interactionCreate', async interaction => {
 
 
 	//Weather Command. Gets user input, pharses it through the weathermap API, returns temperature value
-	if (commandName === 'weather') {
-		const string = options.getString('city');
+	if (commandName === "weather") {
+		const string = options.getString("city");
 
 		await interaction.deferReply({
 			ephemeral: true,
@@ -133,13 +133,13 @@ client.on('interactionCreate', async interaction => {
 			const json = JSON.stringify(smart, null, 2);
 
 			interaction.editReply({
-				content: '>**Location:** ' + string + "\n" +
-				'>**Weather:** '+ smart.temp + 'C\n' +
-				'>**Humidity:** '+ smart.humidity + '%\n' +
-				'>**Pressure:** '+ smart.pressure + '\n' +
-				'>**Description:** '+ smart.description + '\n' +
-				'>**Rain:** ' + smart.rain,
-			})
+				content: ">**Location:** " + string + "\n" +
+				">**Weather:** "+ smart.temp + "C\n" +
+				">**Humidity:** "+ smart.humidity + "%\n" +
+				">**Pressure:** "+ smart.pressure + "\n" +
+				">**Description:** "+ smart.description + "\n" +
+				">**Rain:** " + smart.rain,
+			});
 		});
 	}
 });
